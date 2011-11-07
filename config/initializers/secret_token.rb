e2765a5c5f7873bbc2c2d893784cf45b46bf35a3
Uri Nativ <uri.nativ@klarna.com>
2011-11-07 09:29:20 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '25a74107b68500771d41e3778d9520976b5ef21c69401ac31fd6e788ab55dc6a7847566a451c96b3776290696d09a92a8d16b2b3ab4b09614b6ff628cbdfef34'
